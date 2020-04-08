498c4eb652fd77594c50f875e5acfd7522834e70
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-08 10:43:10 +0900

    header,footerの分割

---
+++ footer.php
@@ -0,0 +1,65 @@
+<footer>
+
+  <div id="footermenu" class="inner">
+    <ul>
+      <li class="title">メニュータイトル</li>
+      <li><a href="index.html">ホーム</a></li>
+      <li><a href="info.html">掲載のご案内</a></li>
+      <li><a href="faq.html">よく頂く質問</a></li>
+      <li><a href="link.html">リンク</a></li>
+      <li><a href="contact.html">お問い合わせ</a></li>
+      <li><a href="company.html">会社概要</a></li>
+    </ul>
+    <ul>
+      <li class="title">情報一覧</li>
+      <li><a href="list_foods.html">飲食店を探す</a></li>
+      <li><a href="list_job.html">求人を探す</a></li>
+      <li><a href="list_clinic.html">病院を探す</a></li>
+      <li><a href="list_school.html">習い事を探す</a></li>
+    </ul>
+    <ul>
+      <li class="title">不動産情報</li>
+      <li><a href="list_fudosan.html">売買物件を探す</a></li>
+      <li><a href="list_fudosan_chintai.html">賃貸物件を探す</a></li>
+    </ul>
+    <ul>
+      <li class="title">メニュータイトル</li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+    </ul>
+    <ul>
+      <li class="title">メニュータイトル</li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+      <li><a href="#">メニューサンプルメニューサンプル</a></li>
+    </ul>
+  </div>
+  <!--/footermenu-->
+
+  <div id="copyright">
+    <small>Copyright&copy; <a href="index.html">ポータルサイト</a> All Rights Reserved.</small>
+    <span class="pr"><a href="https://template-party.com/" target="_blank">《Web Design:Template-Party》</a></span>
+  </div>
+
+</footer>
+
+<!--ページの上部に戻る「↑」ボタン-->
+<p class="nav-fix-pos-pagetop"><a href="#">↑</a></p>
+
+<!--メニュー開閉ボタン-->
+<div id="menubar_hdr" class="close"></div>
+<!--メニューの開閉処理条件設定　900px以下-->
+<script>
+  if (OCwindowWidth() <= 900) {
+    open_close("menubar_hdr", "menubar-s");
+  }
+</script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>ポータルサイト向け 無料ホームページテンプレート tp_portal1</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="ここにサイト説明を入れます">
+  <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <script src="<?php echo get_template_directory_uri(); ?>/js/openclose.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/fixmenu.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/fixmenu_pagetop.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/ddmenu_min.js"></script>
+  <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+  <header>
+    <div class="inner">
+      <h1 id="logo"><a href="<?php echo get_template_directory_uri(); ?>/index.php"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="ポータルサイト"></a></h1>
+      <p class="login"><a href="<?php echo get_template_directory_uri(); ?>/login.html">ログイン</a></p>
+    </div>
+  </header>
+
+  <!--PC用（901px以上端末）メニュー-->
+  <nav id="menubar" class="nav-fix-pos">
+    <ul class="inner">
+      <li><a href="<?php echo get_template_directory_uri(); ?>/index.html">ホーム<span>Home</span></a></li>
+      <li><a href="<?php echo get_template_directory_uri(); ?>/list_base.html">情報一覧<span>Category</span></a>
+        <ul class="ddmenu">
+          <li><a href="<?php echo get_template_directory_uri(); ?>/list_foods.html">飲食店を探す</a></li>
+          <li><a href="<?php echo get_template_directory_uri(); ?>/list_job.html">求人を探す</a></li>
+          <li><a href="<?php echo get_template_directory_uri(); ?>/list_clinic.html">病院を探す</a></li>
+          <li><a href="<?php echo get_template_directory_uri(); ?>/list_school.html">習い事を探す</a></li>
+          <li class="title"><a href="#">不動産情報</a></li>
+          <li><a href="<?php echo get_template_directory_uri(); ?>/list_fudosan.html">売買物件を探す</a></li>
+          <li><a href="<?php echo get_template_directory_uri(); ?>/list_fudosan_chintai.html">賃貸物件を探す</a></li>
+        </ul>
+      </li>
+      <li><a href="<?php echo get_template_directory_uri(); ?>/info.html">掲載のご案内<span>Information</span></a></li>
+      <li><a href="<?php echo get_template_directory_uri(); ?>/faq.html">よく頂く質問<span>Faq</span></a></li>
+      <li><a href="<?php echo get_template_directory_uri(); ?>/link.html">リンク<span>Link</span></a></li>
+      <li><a href="<?php echo get_template_directory_uri(); ?>/contact.html">お問い合わせ<span>Contact</span></a></li>
+    </ul>
+  </nav>
+  <!--小さな端末用（900px以下端末）メニュー-->
+  <nav id="menubar-s">
+    <ul>
+      <li><a href="index.html">ホーム<span>Home</span></a></li>
+      <li><a href="list_base.html">情報一覧<span>Category</span></a></li>
+      <li><a href="info.html">掲載のご案内<span>Information</span></a></li>
+      <li><a href="faq.html">よく頂く質問<span>Faq</span></a></li>
+      <li><a href="link.html">リンク<span>Link</span></a></li>
+      <li><a href="contact.html">お問い合わせ<span>Contact</span></a></li>
+    </ul>
+  </nav>
