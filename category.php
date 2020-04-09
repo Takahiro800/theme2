809395542cfed6351e71177e14a2cfb87a5f01ff
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-09 09:18:20 +0900

    左右の一覧をテンプレート化

--- category.php
+++ category.php
@@ -52,46 +52,7 @@
 		<!--/#main-->
 
 		<div id="sub">
-
-			<nav>
-				<h2>情報一覧</h2>
-				<ul class="submenu">
-					<?php
-					$args = array(
-						'title_li' => '',
-						'exclude' => '6',
-					);
-					wp_list_categories($args); ?>
-				</ul>
-			</nav>
-
-			<nav>
-				<h2>不動産情報</h2>
-				<ul class="submenu">
-					<?php
-					$args = array(
-						'title_li' => '',
-						'exclude' => '1,2,3,4,5,'
-					);
-					wp_list_categories($args); ?>
-				</ul>
-			</nav>
-
-			<div class="box1">
-				<h2 class="mb10">運営会社</h2>
-				<p>東京都XX区XXXXビル３F<br>
-					TEL：00-0000-0000<br>
-					受付：00:00〜00:00<br>
-					定休日：水曜日</p>
-			</div>
-
-			<p>
-				<a href="recruit.html"><img src="<?php echo get_template_directory_uri(); ?>/images/banner1.jpg" alt="採用情報" class="pc"></a>
-				<a href="recruit.html"><img src="<?php echo get_template_directory_uri(); ?>/images/banner1_sh.jpg" alt="採用情報" class="sh"></a>
-			</p>
-			<p>上のバナー画像は、901px以上の端末と900px以下とで画像２種類が切り替わります。<br>
-				<a href="company.html#banner">詳しい説明はこちら。</a></p>
-
+			<?php get_template_part('template-parts', 'sub'); ?>
 		</div>
 		<!--/#sub-->
 
@@ -99,94 +60,7 @@
 	<!--/#contents-in-->
 
 	<div id="side">
-
-		<section>
-			<?php $args = array(
-				'orderby' => 'date',
-				'order' => 'DESC',
-				'tag' => 'recommend',
-			);
-			$posts = get_posts($args);
-			if ($posts) : ?>
-
-				<h2>管理者おすす情報</h2>
-				<?php
-					foreach ($posts as $post) :
-						setup_postdata($post)
-						?>
-					<div class="list-sub">
-						<a href="<?php the_permalink(); ?>" target="_blank">
-							<p class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_yahoo.jpg" alt=""></p>
-							<h4><?php the_title(); ?></h4>
-							<p><?php the_excerpt(); ?></p>
-							<span class="new">new</span>
-						</a>
-					</div>
-				<?php endforeach; ?>
-			<?php endif;
-			wp_reset_postdata(); ?>
-		</section>
-
-		<section>
-
-			<h2>お知らせ</h2>
-
-			<div class="list-sub">
-				<a href="#">
-					<p class="img t"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_side1.jpg" alt="タイトル"></p>
-					<h4>アルバイト募集！</h4>
-					<p>ここに簡単な説明を入れます。サンプルテキスト。</p>
-					<p class="name">サンプルカフェ</p>
-				</a>
-			</div>
-
-			<div class="list-sub">
-				<a href="#">
-					<p class="img t"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_side2.png" alt="タイトル"></p>
-					<h4>医療スタッフ募集</h4>
-					<p>ここに簡単な説明を入れます。サンプルテキスト。</p>
-					<p class="name">サンプルクリニック</p>
-				</a>
-			</div>
-
-			<div class="list-sub">
-				<a href="#">
-					<p class="img t"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_side3.jpg" alt="タイトル"></p>
-					<h4>Cafeオープン記念</h4>
-					<p>ここに簡単な説明を入れます。サンプルテキスト。</p>
-					<p class="name">サンプルCafe</p>
-				</a>
-			</div>
-
-			<div class="list-sub">
-				<a href="#">
-					<p class="img t"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_side1.jpg" alt="タイトル"></p>
-					<h4>アルバイト募集！</h4>
-					<p>ここに簡単な説明を入れます。サンプルテキスト。</p>
-					<p class="name">サンプルカフェ</p>
-				</a>
-			</div>
-
-			<div class="list-sub">
-				<a href="#">
-					<p class="img t"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_side2.png" alt="タイトル"></p>
-					<h4>医療スタッフ募集</h4>
-					<p>ここに簡単な説明を入れます。サンプルテキスト。</p>
-					<p class="name">サンプルクリニック</p>
-				</a>
-			</div>
-
-			<div class="list-sub">
-				<a href="#">
-					<p class="img t"><img src="<?php echo get_template_directory_uri(); ?>/images/sample_side3.jpg" alt="タイトル"></p>
-					<h4>Cafeオープン記念</h4>
-					<p>ここに簡単な説明を入れます。サンプルテキスト。</p>
-					<p class="name">サンプルCafe</p>
-				</a>
-			</div>
-
-		</section>
-
+		<?php get_template_part('template-parts', 'side'); ?>
 	</div>
 	<!--/#side-->
 
