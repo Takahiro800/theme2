d9a8592497dd61514f75e047891db5d03126021b
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-09 10:13:57 +0900

    不要な本文の削除

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
-<div id="contents" class="inner">
 
+<div id="contents" class="inner">
 	<div id="contents-in">
 
 		<aside id="mainimg">
@@ -12,45 +12,7 @@
 		</aside>
 
 		<div id="main">
-
 			<section>
-
-				<h2>テンプレートのご利用前に必ずお読み下さい</h2>
-
-				<h3>利用規約のご案内</h3>
-				<p>このテンプレートは、<a href="https://template-party.com/">Template Party</a>にて無料配布している『ポータルサイト向け 無料ホームページテンプレート tp_portal1』です。必ずダウンロード先のサイトの<a href="https://template-party.com/read.html">利用規約</a>をご一読の上でご利用下さい。</p>
-				<p><strong class="color1">■HP最下部の著作表示『Web Design:Template-Party』は無断で削除しないで下さい。</strong><br>
-					わざと見えなく加工する事も禁止です。</p>
-				<p><strong class="color1">■下部の著作を外したい場合は</strong><br>
-					<a href="https://template-party.com/">Template-Party</a>の<a href="https://template-party.com/member.html">ライセンス契約</a>を行う事でHP下部の著作を外す事ができます。</p>
-
-				<h3>上のスライドショー写真はダウンロードファイルには梱包されていません</h3>
-				<p>配布元である「PIXTA」さんの一般利用規約範囲内で利用している為、ダウンロードファイルには梱包されておりません。PSD特典にも含まれませんので同じ写真が必要な際はPIXTAさんで直接お買い求め下さい。</p>
-
-				<h3>当テンプレートにはお問い合わせフォーム（自動フォーム：試用版）がセットされています</h3>
-				<p><a href="contact.html">contact.html</a>と同じ３項目のお問い合わせフォームを簡単に使えるようにセットしています。</p>
-				<p><strong class="color1">■自動フォームを使う場合（※編集に入る前にご確認下さい）</strong><br>
-					あなたのメールアドレス設定と、簡単な編集だけで使えます。<a href="https://template-party.com/file/formgen_manual_set2.html" target="_blank">詳しくはこちらのマニュアルをご覧下さい。</a></p>
-				<p><strong class="color1">■自動フォームを使わない場合</strong><br>
-					テンプレートに梱包されている「form.html」「confirm.html」「finish.html」の3枚のファイルを削除して下さい。</p>
-
-				<h3>メニューなどの開閉ブロック（openclose.js）について</h3>
-				<p>当テンプレートの開閉ブロックパーツは<a href="https://www.crytus.info/" target="_blank">クリタス様</a>提供のプログラムを使用しています。openclose.jsファイルは改変せずにご利用下さい。<br>
-					また、当サイトのテンプレート以外に使いたいなど、「プログラムのみ」を使う場合は<a href="https://template-party.com/free_program/openclose_license.html">こちらの規約</a>をお守り下さい。</p>
-
-				<h3>当テンプレートの詳しい使い方は</h3>
-				<p><a href="company.html">こちらをご覧下さい。</a></p>
-
-			</section>
-
-			<section>
-				<h2>ポータルサイト向けCMS販売中<span>CMS</span></h2>
-				<p>複数のユーザーが情報を登録し、訪問客がそれを閲覧するポータルサイト向けCMS販売中です。<br>
-					<a href="https://template-party.com/portal_db/"><img src="<?php echo get_template_directory_uri(); ?>/https://template-party.com/portal_db/images/img01.jpg" alt=""></a></p>
-			</section>
-
-			<section>
-
 				<h2>地域の情報検索サイト</h2>
 				<ul class="list2">
 					<li><a href="list_foods.html"><img src="<?php echo get_template_directory_uri(); ?>/images/mark_foods.png" alt="" class="img">飲食店を調べる<br><span>XX件</span></a></li>
